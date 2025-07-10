f36667b975ad04e5816d192626d797f3122ddcf9
dewhizz <lewiskimari44@gmail.com>
2025-07-10 09:37:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// entry file
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+const { json } = require('body-parser')
+require('dotenv').config()
+
+const app =express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// static file accessibility
+app.use('/uploads',express.static('uploads'))
+
+// connection to the db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err=>console.log("MongoDB connection error",err))
+
+// listener
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log(`Server Running On Port ${PORT}`)
+})
